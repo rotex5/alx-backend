89387c1f8226428994fe283a5f234b033636c200
rotex <dantejake48@gmail.com>
2023-05-17 22:22:19 +0100

    fix pycodestyle issue

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -21,13 +21,16 @@ class LRUCache(BaseCaching):
         if key and item:
             # Check if key already exists in the cache
             if key in self.cache_data:
-                self.used_keys.remove(key)  # Remove key from the used_keys
+                # Remove key from the order
+                self.used_keys.remove(key)
             elif len(self.cache_data) >= super().MAX_ITEMS:
-                lru_key = self.used_keys.pop(0)  # Remove the least recently used key
+                # Remove the least recently used key
+                lru_key = self.used_keys.pop(0)
                 print("DISCARD:", lru_key)
                 del self.cache_data[lru_key]
 
-            self.used_keys.append(key)  # Add key to the end of the order
+            # Add key to the end of the order
+            self.used_keys.append(key)
             self.cache_data[key] = item
 
     def get(self, key):
