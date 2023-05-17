405a9b430cff8629246f72d690ed36653f08e190
rotex <dantejake48@gmail.com>
2023-05-17 17:22:49 +0100

    FIFO caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+FIFO Caching
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    Cache implememtation based on FIFO algorithm
+    """
+    def __init__(self):
+        """Constructor"""
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Insert data into cache
+        """
+        if (key is None or item is None):
+            pass
+
+        if (len(self.cache_data) >= BaseCaching.MAX_ITEMS):
+            if (key in self.cache_data):
+                self.cache_data[key] = item
+            else:
+                first_key = list(self.cache_data.keys())[0]
+                print("DISCARD: {}".format(first_key))
+                self.cache_data.pop(first_key)
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        return corresponding value
+        """
+        if (not key or key not in self.cache_data.keys()):
+            return None
+        return self.cache_data.get(key)
