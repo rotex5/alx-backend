7656422252a805ee6e415b3f5f2b77448017cfe7
rotex <dantejake48@gmail.com>
2023-05-17 04:21:06 +0100

    Basic dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Implementing a Cache using a Basic dictionary
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    Class Implementation of a Basic Cache
+    """
+
+    def put(self, key, item):
+        """
+        inserting data into cache dictionay
+        """
+        if (not key or not item):
+            pass
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        return data associated with key
+        """
+        if (not key or key not in self.cache_data.keys()):
+            return None
+        return self.cache_data.get(key)
