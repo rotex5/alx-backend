493825525828de902253f71f57fc20aaf50dd822
rotex <dantejake48@gmail.com>
2023-05-18 05:18:07 +0100

    module level import not at top of file

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -15,7 +15,7 @@ class LFUCache(BaseCaching):
         Insert data into cache
         """
         super().__init__()
-        self.frequency = defaultdict(int)
+        self.item_freq = defaultdict(int)
 
     def put(self, key, item):
         """
@@ -24,13 +24,13 @@ class LFUCache(BaseCaching):
         if key and item:
             if key in self.cache_data:
                 self.cache_data[key] = item
-                self.frequency[key] += 1
+                self.item_freq[key] += 1
             else:
                 if len(self.cache_data) >= super().MAX_ITEMS:
                     self.discard_least_frequent()
 
                 self.cache_data[key] = item
-                self.frequency[key] = 1
+                self.item_freq[key] = 1
 
     def get(self, key):
         """
@@ -38,7 +38,7 @@ class LFUCache(BaseCaching):
         """
         if key is None or key not in self.cache_data:
             return None
-        self.frequency[key] += 1
+        self.item_freq[key] += 1
         return self.cache_data.get(key)
 
     def discard_least_frequent(self):
@@ -46,20 +46,20 @@ class LFUCache(BaseCaching):
         selects the least frequent key for eviction
         based on the frequency values
         """
-        min_freq = min(self.frequency.values())
+        min_freq = min(self.item_freq.values())
 
-        min_freq_keys = [k for k, v in self.frequency.items() if v == min_freq]
+        min_freq_keys = [k for k, v in self.item_freq.items() if v == min_freq]
 
         if len(min_freq_keys) == 1:
             key_to_discard = min_freq_keys[0]
         else:
             lru_key = min_freq_keys[0]
             for key in min_freq_keys:
-                if self.frequency[key] < self.frequency[lru_key]:
+                if self.item_freq[key] < self.item_freq[lru_key]:
                     lru_key = key
             key_to_discard = lru_key
 
         del self.cache_data[key_to_discard]
-        del self.frequency[key_to_discard]
+        del self.item_freq[key_to_discard]
 
         print("DISCARD: {}".format(key_to_discard))
