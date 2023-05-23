c30ba01026fceb3653eb3808ff3ca12aad91aa04
rotex <dantejake48@gmail.com>
2023-05-23 20:35:53 +0100

    compile babel

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -57,6 +57,9 @@ def get_locale() -> str:
         return locale
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
+# babel.init_app(app, locale_selector=get_locale)
+# @babel.localeselector has issues on this verion
+
 
 @app.route("/", strict_slashes=False)
 def index() -> str:
