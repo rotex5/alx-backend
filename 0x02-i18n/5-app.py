7c8359715e4f09d51c053bcf7b2deda1c6eeecd8
rotex <dantejake48@gmail.com>
2023-05-23 20:19:03 +0100

    Mock logging in

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+"""
+Force locale with URL parameter
+"""
+from flask import Flask, g, render_template, request
+from flask_babel import Babel
+from typing import Union
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config(object):
+    """
+    Config class for Babel
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__, template_folder="templates")
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+def get_user(login_as: int) -> Union[dict, None]:
+    """
+    Returns: user dict or None if ID cannot be found
+    or if login_as was not passed.
+    """
+    if login_as and int(login_as) in users:
+        return users[int(login_as)]
+    return None
+
+
+@app.before_request
+def before_request():
+    """
+    Add user to flask.g if user is found
+    """
+    g.user = get_user(request.args.get('login_as'))
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Get locale from request
+    """
+    locale = request.args.get("locale")
+    if locale and locale in app.config["LANGUAGES"]:
+        return locale
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/", strict_slashes=False)
+def index() -> str:
+    """
+    Returns 5-index.html
+    """
+    return render_template("5-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
