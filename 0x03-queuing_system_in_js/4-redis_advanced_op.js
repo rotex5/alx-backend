f7e9f4d6cd146839a83db85044353bfcd7e0fd37
rotex <dantejake48@gmail.com>
2023-06-21 20:52:10 +0100

    Node Redis client and advanced operations

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,24 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+const client = redis.createClient();
+
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+}).on('error', (error) => {
+  console.log(`Redis client not connected to the server: ${error.message}`);
+});
+
+const KEY = 'HolbertonSchools';
+
+const keys = ['Portland', 'Seattle', 'New York', 'Bogota', 'Cali', 'Paris'];
+const values = [50, 80, 20, 20, 40, 2];
+
+keys.forEach((key, index) => {
+  client.hset(KEY, key, values[index], redis.print);
+});
+
+client.hgetall(KEY, (err, value) => {
+  console.log(value);
+});
